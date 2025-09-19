f56a3831abcd82bfc5abf2a69fcd7121fba663a9
Londongrad <Teslagradus@gmail.com>
2025-09-19 16:05:17 +0900

    Добавьте файлы проекта.

---
+++ AdvertisingService/Controllers/PlatformController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
